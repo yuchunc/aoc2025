15bada49e3be4e662e8661b047fba87fabbd800d
Mickey Chen <hey.mickabo@gmail.com>
2025-12-03 10:17:25 +0800

    initial commit

---
+++ lib/aoc2025.ex
@@ -0,0 +1,18 @@
+defmodule Aoc2025 do
+  @moduledoc """
+  Documentation for `Aoc2025`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Aoc2025.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/aoc2025_test.exs
@@ -0,0 +1,8 @@
+defmodule Aoc2025Test do
+  use ExUnit.Case
+  doctest Aoc2025
+
+  test "greets the world" do
+    assert Aoc2025.hello() == :world
+  end
+end
